854b4f22396d229529d3fc263ad6f28af0cbf086
Yash <yashchand17@gmail.com>
2024-05-07 20:44:13 +0530

    Final Commit

---
+++ components/ProductsGrid.js
@@ -0,0 +1,24 @@
+import styled from "styled-components";
+import ProductBox from "./ProductBox";
+
+const StyledProductsGrid = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  gap: 20px;
+  @media screen and (min-width: 768px) {
+    grid-template-columns: 1fr 1fr 1fr 1fr;
+  }
+`;
+
+export default function ProductsGrid({ products }) {
+  return (
+    <StyledProductsGrid>
+      {products?.length > 0 &&
+        products.map((product) => (
+          <div>
+            <ProductBox key={product._id} {...product} />
+          </div>
+        ))}
+    </StyledProductsGrid>
+  );
+}
